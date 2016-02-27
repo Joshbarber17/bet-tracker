c46969762d48a9052d5887dcb81d322c385fc36d
Josh Barber <JoshsMacbook@Joshs-MacBook-Pro.local>
2016-02-26 17:57:48 -0700

    got a little bit of my functionality working

--- js/controllers/bettersCtrl.js
+++ js/controllers/bettersCtrl.js
@@ -1,3 +1,12 @@
 angular.module('betTrack').controller('bettersCtrl', function($scope){
+  $scope.array = [];
+  $scope.newBetter = function(firstName, lastName, phone, payId) {
+    $scope.array.push({
+      firstName: firstName,
+      lastName: lastName,
+      phone: phone,
+      payId: payId
+    })
+  }
   
 })
